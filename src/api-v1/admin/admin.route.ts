576a75f56524d07fe41a8f5c6e5223b3d9c9ebc2
veroni-d20 <veronishwetha.23it@licet.ac.in>
2022-02-06 17:25:06 +0530

    get eventId

--- src/api-v1/admin/admin.route.ts
+++ src/api-v1/admin/admin.route.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import AttendanceController from "./attendance.controller";
 
-const users: Router = Router();
+const admin: Router = Router();
 const attendanceController = new AttendanceController();
 
 // Retrieve all Users
-users.route("/attendance").get(attendanceController.getAttendance);
+admin.route("/attendance").get(attendanceController.getAttendance);
 
-export default users;
+export default admin;
