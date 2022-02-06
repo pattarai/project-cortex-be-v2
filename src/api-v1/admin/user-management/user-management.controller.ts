8000669f620b8d35ba6139d96cdd4bad8d8364c0
Joshua Frankle <joshuafrankle7@gmail.com>
2022-02-06 21:28:13 +0530

    Added user-management

---
+++ src/api-v1/admin/user-management/user-management.controller.ts
@@ -0,0 +1,101 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default class UsermanagementController {
+  public getUser = async (req: Request, res: Response): Promise<any> => {
+    try {
+      const user = await prisma.users.findMany({
+        select: {
+          userId: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          project: true,
+          committee: true,
+          startDate: true,
+          isActive: true,
+        },
+      });
+      return res.status(200).json({
+        message: "Success",
+        user,
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({
+        success: false,
+        message: e.toString(),
+      });
+    }
+  };
+
+  public createUser = async (req: Request, res: Response): Promise<any> => {
+    try {
+      const userDetails = req.body;
+      const user = await prisma.users.create({
+        data: {
+          ...userDetails,
+        },
+      });
+      return res.status(200).json({
+        message: "Success",
+        user,
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({
+        success: false,
+        message: e.toString(),
+      });
+    }
+  };
+
+  public updateUser = async (req: Request, res: Response): Promise<any> => {
+    try {
+      const { userId, ...details } = req.body;
+      const user = await prisma.users.update({
+        where: {
+          userId,
+        },
+        data: {
+          ...details,
+        },
+      });
+      return res.status(200).json({
+        message: "Success",
+        user,
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({
+        success: false,
+        message: e.toString(),
+      });
+    }
+  };
+
+  public deleteUser = async (req: Request, res: Response): Promise<any> => {
+    try {
+      const { userId } = req.body;
+      await prisma.users.update({
+        where: {
+          userId,
+        },
+        data: {
+          isActive: false,
+        },
+      });
+      return res.status(200).json({
+        message: "Success",
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({
+        success: false,
+        message: e.toString(),
+      });
+    }
+  };
+}
