fcd61c7fecb5b1bc120fd2099d5475f38df5be68
dhivya910 <dhivyadharshni910@gmail.com>
2022-02-06 17:02:56 +0530

    user attendance init

---
+++ src/api-v1/users/ranks.controller.ts
@@ -0,0 +1,54 @@
+//import * as bcrypt from 'bcrypt';
+import { Request, Response } from "express";
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default class RanksController {
+  public getAllRanks = async (req: Request, res: Response): Promise<any> => {
+    try {
+      const users = await prisma.users.findMany({
+        select: {
+          userId: true,
+          firstName: true,
+          lastName: true,
+          bitmojiUrl: true,
+        },
+      });
+      return res.status(200).json({
+        message: "Success",
+        users,
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({
+        success: false,
+        message: e.toString(),
+      });
+    }
+  };
+  
+  public getUserRanks = async (req: Request, res: Response): Promise<any> => {
+    try {
+      const { userId, phase } = req.body;
+      const ranks = await prisma.ranking.findMany({
+        where: {
+          userId,
+        },
+        select: {
+        score: true,
+        },
+      });
+      return res.status(200).json({
+        message: "Success",
+        ranks,
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({
+        success: false,
+        message: e.toString(),
+      });
+    }
+  };
+}
--- src/api-v1/users/users.route.ts
+++ src/api-v1/users/users.route.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import Controller from "./users.controller";
+import CommonViewController from "./commonView.controller";
+import RanksController from "./ranks.controller";
 
 const users: Router = Router();
-const controller = new Controller();
+const commonViewController = new CommonViewController();
+const ranksController = new RanksController();
 
 // Retrieve all Users
-users.get("/", controller.getAllUsers);
+users.get("/commonView", commonViewController.getUsers);
+users.route("/ranks").get(ranksController.getAllRanks).post(ranksController.getUserRanks);
 
 export default users;
