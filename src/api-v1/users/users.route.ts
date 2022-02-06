cb8a9cfa9a95a1a5b6bd143a667960bfa386e187
Pinto565 <pintoinfant5650@gmail.com>
2022-02-06 22:08:50 +0530

    removed fileupload from users route

--- src/api-v1/users/users.route.ts
+++ src/api-v1/users/users.route.ts
@@ -4,11 +4,8 @@ import commonView from "./common-view/common-view.route";
 import completeProfile from "./complete-profile/complete-profile.route";
 import ranks from "./ranks/ranks.route";
 
-import * as fileUpload from "express-fileupload";
-
 const users: Router = Router();
 
-users.use(fileUpload());
 // Retrieve all Users
 users.use("/common-view", commonView);
 users.use("/ranks", ranks);
