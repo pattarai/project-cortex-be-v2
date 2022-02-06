914cc735ffa3b4dce9b0614e2646672b06c97151
RAKSHA V G <vgraksha26@gmail.com>
2022-02-06 16:39:56 +0530

    Add ranks route

--- src/api-v1/users/users.route.ts
+++ src/api-v1/users/users.route.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import CommonViewController from "./commonView.controller";
+import RanksController from "./ranks.controller";
 
 const users: Router = Router();
 const commonViewController = new CommonViewController();
+const ranksController = new RanksController();
 
 // Retrieve all Users
 users.get("/commonView", commonViewController.getUsers);
+users.route("/ranks").get(ranksController.getAllRanks).post(ranksController.getUserRanks);
 
 export default users;
