fc6daa10c83cbc247bf712b9b8718c6918b30309
dhivya910 <dhivyadharshni910@gmail.com>
2022-02-06 17:23:14 +0530

    events admin

--- src/api-v1/admin/admin.route.ts
+++ src/api-v1/admin/admin.route.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import AttendanceController from "./attendance.controller";
+import EventsController from "./events.controller";
 
 const users: Router = Router();
 const attendanceController = new AttendanceController();
+const eventsController = new EventsController();
 
 // Retrieve all Users
 users.route("/attendance").get(attendanceController.getAttendance);
+users.route("/events").get(eventsController.getEvents);
 
 export default users;
