2b41a3858d913d233a3ee91cb4dc22683000eb74
Joshua Frankle <joshuafrankle7@gmail.com>
2022-02-06 21:56:57 +0530

    made root route

--- src/api-v1/admin/user-management/user-management.route.ts
+++ src/api-v1/admin/user-management/user-management.route.ts
@@ -5,7 +5,7 @@ const users: Router = Router();
 const userManagementController = new userManagement();
 
 users
-  .route("/usermanagement")
+  .route("/")
   .get(userManagementController.getUser)
   .post(userManagementController.createUser)
   .patch(userManagementController.updateUser)
