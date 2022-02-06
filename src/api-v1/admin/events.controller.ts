79bdd6236d874fc5374c8e377cb005f2a6848fe6
dhivya910 <dhivyadharshni910@gmail.com>
2022-02-06 19:47:37 +0530

    events admin done

--- src/api-v1/admin/events.controller.ts
+++ src/api-v1/admin/events.controller.ts
@@ -8,7 +8,7 @@ export default class EventsController {
   public getEvents = async (req: Request, res: Response): Promise<any> => {
     try {
       const { phase } = req.body;
-      const eventId = await prisma.events.findMany({
+      const _events = await prisma.events.findMany({
         where: { phase },
         select: {
           eventName: true,
@@ -18,6 +18,59 @@ export default class EventsController {
       });
       return res.status(200).json({
         message: "Success",
+        _events,
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({
+        success: false,
+        message: e.toString(),
+      });
+    }
+  };
+
+  public postEvent = async (req: Request, res: Response): Promise<any> => {
+    try {
+      const { eventName, eventType, eventDate,conductedBy,name, phase } = req.body;
+      const _events = await prisma.events.create({
+        data: {
+          eventName,
+          eventType,
+          eventDate,
+          conductedBy,
+          name,
+          phase,
+        }
+      });
+      return res.status(200).json({
+        success: true,
+        _events,
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({
+        success: false,
+        message: e.toString(),
+      });
+    }
+  };
+
+  public deleteEvent = async (req: Request, res: Response): Promise<any> => {
+    try {
+      const { eventId } = req.body;
+      const eventName = await prisma.events.delete({
+        where: {eventId},
+        select: {
+          eventName: true,
+          eventType: true,
+          eventDate: true,
+          name: true,
+          conductedBy: true,
+          phase: true,
+        }
+      });
+      return res.status(200).json({
+        success: true,
         eventId,
       });
     } catch (e) {
@@ -28,4 +81,33 @@ export default class EventsController {
       });
     }
   };
+
+  public patchEvent = async (req: Request, res: Response): Promise<any> => {
+    try {
+      const { eventId, eventName, eventType, eventDate,conductedBy,name, phase } = req.body;
+      const  _events= await prisma.events.update({
+        where: {eventId},
+        data: {
+          eventId,
+          eventName,
+          eventType,
+          eventDate,
+          conductedBy,
+          name,
+          phase,
+        },
+      });
+      return res.json({
+        success: true,
+        data: _events,
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({
+        success: false,
+        message: e.toString(),
+      });
+    }
+  }
+
 }
