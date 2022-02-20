4b6d9df5dce4c4bf261edfd11b031840d0eecaef
Pinto565 <pintoinfant5650@gmail.com>
2022-02-20 21:24:48 +0530

    middlewares

---
+++ src/middlewares/validateUser.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express"
+import { verify } from "jsonwebtoken"
+
+export const validateUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    let token = req.headers.authorization?.split(" ")[1]
+    let jwtSecret = process.env.JWT_AUTH_SECRET
+    if (token) {
+        verify(token, jwtSecret, (err, decoded: any) => {
+            if (err) {
+                res.json({
+                    status: false,
+                    message: "Invalid token"
+                })
+            } else {
+                res.locals.user = decoded.payload
+                next()
+            }
+        })
+    } else {
+        res.json({
+            status: false,
+            message: "Please provide token"
+        })
+    }
+}
