8eac99917052b33880d6dc76d49325137b1c5400
Elizabeth Subhiksha <esubivictoria129@gmail.com>
2022-03-16 22:26:25 +0530

    IsCompleted added to Complete profile

--- src/api-v1/users/complete-profile/complete-profile.controller.ts
+++ src/api-v1/users/complete-profile/complete-profile.controller.ts
@@ -7,110 +7,110 @@ import * as path from "path";
 const prisma = new PrismaClient();
 
 export default class CompleteProfile {
-    public completeProfileGet = async (
-        req: Request,
-        res: Response
-    ): Promise<any> => {
-        try {
-            const { userId } = req.body;
-            const user = await prisma.users.findFirst({
-                where: {
-                    userId: 7,
-                },
-                select: {
-                    userId: true,
-                    firstName: true,
-                    lastName: true,
-                    email: true,
-                    committee: true,
-                    project: true,
-                    roleId: true,
-                    dateOfBirth: true,
-                    collegeName: true,
-                    department: true,
-                    year: true,
-                    rollNumber: true,
-                    registerNumber: true,
-                    whatsappNumber: true,
-                    instagramUrl: true,
-                    githubUrl: true,
-                    linkedInUrl: true,
-                    description: true,
-                },
-            });
-            return res.json({
-                success: true,
-                user,
-            });
-        } catch (err) {
-            return res.status(500).json({
-                success: false,
-                message: err.toString(),
-            });
-        }
-    };
+  public completeProfileGet = async (
+    req: Request,
+    res: Response
+  ): Promise<any> => {
+    try {
+      const { userId } = req.body;
+      const user = await prisma.users.findFirst({
+        where: {
+          userId: 7,
+        },
+        select: {
+          userId: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          committee: true,
+          project: true,
+          roleId: true,
+          dateOfBirth: true,
+          collegeName: true,
+          department: true,
+          year: true,
+          rollNumber: true,
+          registerNumber: true,
+          whatsappNumber: true,
+          instagramUrl: true,
+          githubUrl: true,
+          linkedInUrl: true,
+          description: true,
+        },
+      });
+      return res.json({
+        success: true,
+        user,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: err.toString(),
+      });
+    }
+  };
 
-    public completeProfilePatch = async (
-        req: Request,
-        res: Response
-    ): Promise<any> => {
-        try {
-            const {
+  public completeProfilePatch = async (
+    req: Request,
+    res: Response
+  ): Promise<any> => {
+    try {
+      const {
+        dateOfBirth,
+        collegeName,
+        department,
+        year,
+        rollNumber,
+        registerNumber,
+        whatsappNumber,
+        instagramUrl,
+        githubUrl,
+        linkedInUrl,
+        description,
+      } = req.body;
 
-                dateOfBirth,
-                collegeName,
-                department,
-                year,
-                rollNumber,
-                registerNumber,
-                whatsappNumber,
-                instagramUrl,
-                githubUrl,
-                linkedInUrl,
-                description,
-            } = req.body;
+      let userId = 7;
 
-            let userId = 7;
+      let profilePicFile = req.files.profilePic as UploadedFile;
+      let bitmojiFile = req.files.bitmojiPic as UploadedFile;
 
-            let profilePicFile = req.files.profilePic as UploadedFile;
-            let bitmojiFile = req.files.bitmojiPic as UploadedFile;
+      if (profilePicFile && bitmojiFile) {
+        let profilePath = path.join("public", "profile", `${userId}.jpg`);
+        let bitmojiPath = path.join("public", "bitmoji", `${userId}.jpg`);
 
-            if (profilePicFile && bitmojiFile) {
-                let profilePath = path.join("public", "profile", `${userId}.jpg`);
-                let bitmojiPath = path.join("public", "bitmoji", `${userId}.jpg`);
+        profilePicFile.mv(profilePath);
+        bitmojiFile.mv(bitmojiPath);
+      }
 
-                profilePicFile.mv(profilePath);
-                bitmojiFile.mv(bitmojiPath);
-            }
+      const user = await prisma.users.update({
+        where: {
+          userId: Number(userId),
+        },
+        data: {
+          dateOfBirth: new Date(dateOfBirth),
+          collegeName,
+          department,
+          year: Number(year),
+          rollNumber,
+          registerNumber,
+          whatsappNumber,
+          instagramUrl,
+          githubUrl,
+          linkedInUrl,
+          description,
+          isCompleted: true,
+        },
+      });
 
-            const user = await prisma.users.update({
-                where: {
-                    userId: Number(userId),
-                },
-                data: {
-                    dateOfBirth: new Date(dateOfBirth),
-                    collegeName,
-                    department,
-                    year: Number(year),
-                    rollNumber,
-                    registerNumber,
-                    whatsappNumber,
-                    instagramUrl,
-                    githubUrl,
-                    linkedInUrl,
-                    description,
-                },
-            });
-
-            return res.json({
-                success: true,
-                user,
-            });
-        } catch (err) {
-            return res.status(500).json({
-                success: false,
-                message: err.toString(),
-            });
-        }
-    };
+      return res.json({
+        success: true,
+        user,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: err.toString(),
+      });
+    }
+  };
 }
