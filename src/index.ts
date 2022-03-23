5418dcf59a862c577b41d98fa47453092cbfe433
Pinto565 <pintoinfant5650@gmail.com>
2022-03-23 19:30:10 +0530

    added ssl cert

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import * as https from "https";
+import * as fs from "fs";
+import * as path from "path"
+
 import server from "./api";
 
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-server.listen(process.env.API_PORT || "5000", () => {
+const httpServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, "..", "selfsigned.key")),
+  cert: fs.readFileSync(path.join(__dirname, "..", "selfsigned.crt"))
+}, server);
+
+httpServer.listen(process.env.API_PORT || "5000", () => {
   console.log(
-    `The API server has successfully started. \nListening at ${
-      process.env.APP_BASE_URL || "http://localhost:5000"
+    `The API server has successfully started. \nListening at ${process.env.APP_BASE_URL || "http://localhost:5000"
     }`
   );
 });
