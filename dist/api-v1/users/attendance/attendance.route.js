7560371c0064a6c6f0d7a30f2a0dd906e6a95304
Pinto565 <pintoinfant5650@gmail.com>
2022-04-02 20:39:19 +0530

    removed build folder

--- dist/api-v1/users/attendance/attendance.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const attendance_controller_1 = require("./attendance.controller");
-const attendance = (0, express_1.Router)();
-const attendanceController = new attendance_controller_1.default();
-attendance.route("/").post(attendanceController.getAttendance);
-exports.default = attendance;
-//# sourceMappingURL=attendance.route.js.map
